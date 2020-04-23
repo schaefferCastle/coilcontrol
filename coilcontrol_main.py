a1bc4b5fa54549baa701ae3a89190ec082a82079
schaefferCastle <44036656+schaefferCastle@users.noreply.github.com>
2020-04-23 10:40:46 +0200

    implemented smooth current ramp and lin ramp with target

--- coilcontrol_main.py
+++ coilcontrol_main.py
@@ -1,9 +1,35 @@
+# Copyright (c) 2020, A.Tom <thomas.astner@univie.ac.at>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# -------------------------------------------------------------------------------
+#  Control a Aim TTi PL-P Series DC Power Supplies - Perform Current Ramps
+# -------------------------------------------------------------------------------
+
 import sys
 from os import environ
 from random import randint
 import time
 import traceback
 import threading
+import numpy as np
+import matplotlib.pyplot as plt
 
 import pyqtgraph as pg
 from PyQt5 import QtWidgets, QtCore
@@ -25,16 +51,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.threadpool = QThreadPool()
         print("Multithreading with maximum %d threads started" % self.threadpool.maxThreadCount())
 
-
         # Plot update timer
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(50)
+        self.timer.setInterval(25)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
-        self.hour = list(range(100))
-        self.temperature = [randint(0, 2) for _ in range(100)]
-        self.temperature2 = [randint(0, 2) for _ in range(100)]
+        self.hour = list(range(500))
+        self.temperature = [randint(0, 2) for _ in range(500)]
+        self.temperature2 = [randint(0, 2) for _ in range(500)]
 
         # PLOT Window 1
         self.plot_1.setBackground('w')
@@ -55,9 +80,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.plot_2.addLegend()
         self.plot_2.showGrid(x=True, y=True)
         self.my_line_ref2 = self.plot_2.plot(self.hour, self.temperature2, name='Sensor 2', pen=pen)
+        self.plot_2.setYRange(0, 5)
 
         # Signals and stuff
-        self.pushButton.clicked.connect(self.on_click)
         self.connected = False
         self.checkBox_connect.stateChanged.connect(self.device_connect)
 
@@ -70,6 +95,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.outputvar = False
         self.automaticstatevar = False
         self.autostatvar = False
+        self.rampinprogress = False
 
         self.checkBox_output.stateChanged.connect(lambda: self.chkstate(self.checkBox_output))
         self.pushButton_setval.clicked.connect(lambda: self.clickevents(self.pushButton_setval))
@@ -84,6 +110,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # self.ramptimer = QtCore.QTimer()
         # self.ramptimer.timeout.connect(self.rampcurrent)
         self.pushButton_startramp.clicked.connect(lambda: self.clickevents(self.pushButton_startramp))
+        self.smoothDwell = 100
+        self.ramppoints = []
+
+        self.pushButton_showtrajectorie.clicked.connect(lambda: self.clickevents(self.pushButton_showtrajectorie))
+        self.pushButton_smoothrampstart.clicked.connect(lambda: self.clickevents(self.pushButton_smoothrampstart))
+
+        self.pushButton_linrampshowtrajectory.clicked.connect(lambda:
+                                                              self.clickevents(self.pushButton_linrampshowtrajectory))
+        self.pushButton_linrampstart.clicked.connect(lambda: self.clickevents(self.pushButton_linrampstart))
 
     def clickevents(self, b):
         if b.objectName() == 'pushButton_setval':
@@ -124,16 +159,43 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 print('Timer stopped!')
         if b.objectName() == 'pushButton_startramp':
             print('Starting the ramp!')
-            # worker = Worker(self.rampcurrent(delta=self.doubleSpinBox_autoincr.value(),
-            #                                  target=self.doubleSpinBox_target.value(),
-            #                                  waittime=self.spinBox_dwell.value()/1000, statvar=self.autostatvar))
-            # worker.signals.finished.connect(self.thread_complete)
-            self.timer.stop()
-            t = threading.Thread(target=self.rampcurrent, args=(self.doubleSpinBox_autoincr.value(),
-                                             self.doubleSpinBox_target.value(),
-                                             self.spinBox_dwell.value()/1000, self.autostatvar))
+            if not self.rampinprogress:
+                self.rampinprogress = True
+                # worker = Worker(self.rampcurrent(delta=self.doubleSpinBox_autoincr.value(),
+                #                                  target=self.doubleSpinBox_target.value(),
+                #                                  waittime=self.spinBox_dwell.value()/1000, statvar=self.autostatvar))
+                # worker.signals.finished.connect(self.thread_complete)
+                # self.timer.stop()
+                t = threading.Thread(target=self.rampcurrent, args=(self.doubleSpinBox_autoincr.value(),
+                                                                    self.doubleSpinBox_target.value(),
+                                                                    self.spinBox_dwell.value() / 1000,
+                                                                    self.autostatvar))
+                t.start()
+                # self.threadpool.start(worker)
+            else:
+                print('You can not start a second ramp thread!')
+        if b.objectName() == 'pushButton_showtrajectorie':
+            result = self.calcTrajectorie()
+            self.ramppoints = result[1]
+            self.smoothDwell = result[0]
+            # print(self.ramppoints)
+        if b.objectName() == 'pushButton_smoothrampstart':
+            print('Starting smooth ramp!')
+            result = self.calcTrajectorie()
+            self.ramppoints = result[1]
+            self.smoothDwell = result[0]
+            t = threading.Thread(target=self.rampcurrentlist)
             t.start()
-            # self.threadpool.start(worker)
+        if b.objectName() == 'pushButton_linrampshowtrajectory':
+            result = self.calcLinTrajectorie()
+            self.ramppoints = result[1]
+            self.smoothDwell = result[0]
+        if b.objectName() == 'pushButton_linrampstart':
+            result = self.calcLinTrajectorie()
+            self.ramppoints = result[1]
+            self.smoothDwell = result[0]
+            t = threading.Thread(target=self.rampcurrentlist)
+            t.start()
 
     def chkstate(self, b):
         if b.objectName() == 'checkBox_output':
@@ -158,7 +220,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.checkBox_output.setEnabled(False)
 
     def rampcurrent(self, delta, target, waittime, statvar):
-        current = self.supply.measureCurrent(channel=2)
+        self.rampinprogress = True
+        current = self.supply.queryCurrent(channel=2)
+        # self.timer.stop()
         if statvar is False:
             print('Deciding direction of ramp...')
             statvar = True
@@ -175,21 +239,31 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             while current < target:
                 self.supply.incCurrentByDelta(channel=2)
                 time.sleep(waittime)
-                if current >= 0.99 * target:
-                    print('Reached target current value! Stopping ramp!')
+                if current > target:
+                    print('Current larger than target, breaking!')
                     break
-                current = self.supply.measureCurrent(channel=2)
-                print(current)
+                current = self.supply.queryCurrent(channel=2)
+                # print(current)
         if rampdirection > 0:
             while current > target:
                 self.supply.decCurrentByDelta(channel=2)
                 time.sleep(waittime)
-                if current <= 0.99 * target:
-                    print('Reached target current value! Stopping ramp!')
+                if current < target:
+                    print('Current smaller than target, breaking!')
                     break
-                current = self.supply.measureCurrent(channel=2)
-                print(current)
+                current = self.supply.queryCurrent(channel=2)
+                # print(current)
+        print('Reached target vlue!')
+        self.rampinprogress = False
         return 1
+
+    def rampcurrentlist(self):
+        print('Smooth current ramp starting!')
+        for i in self.ramppoints:
+            print(i)
+            self.supply.setCurrent(i, channel=2, wait=0)
+            time.sleep(self.smoothDwell)
+            print(i)
 
     def thread_complete(self):
         print('Thread completed!')
@@ -203,7 +277,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.temperature = self.temperature[1:]
         self.temperature2 = self.temperature2[1:]
-        if self.connected:
+        if self.connected and not self.rampinprogress:
             a = self.supply.measureCurrent(channel=2)
             b = self.supply.measureVoltage(channel=2)
             self.temperature.append(b)
@@ -220,11 +294,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def plot(self, hour, temperature, **kwargs):
         my_line_ref = self.plot_1.plot(hour, temperature, **kwargs)
         return my_line_ref
-
-    def on_click(self):
-        print('Button clicked!')
-        a = self.supply.queryCurrentDelta(channel=2)
-        self.label_status.setText(str(a))
 
     def closeEvent(self, event):
         if self.connected:
@@ -257,6 +326,77 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         if b.text() == "Voltage" and b.isChecked():
             self.modevariable = True
             print('voltage branch', self.modevariable)
+
+    def calcTrajectorie(self):
+        print('Calculating current trajectorie!')
+        if self.checkBox_connect.isChecked() == True:
+            currentnow = self.supply.measureCurrent(channel=2)
+        else:
+            currentnow = 0.
+
+        target = self.doubleSpinBox_smoothtarget.value()
+        y3 = abs(currentnow - target)
+        rate = self.doubleSpinBox_smoothrate.value() / 60.
+        dwell = self.spinBox_smoothdwell.value() / 1000.
+
+        x3 = y3 / rate  # ramp time
+        reso = x3 / dwell
+        # print('Ramp contains {} points'.format(reso))
+
+        x = np.linspace(0, x3, int(reso))
+        y = np.piecewise(x, [x < x3 / 3, (x3 / 3 <= x) * (x < 2 * x3 / 3), (2 * x3 / 3 <= x) * (x <= x3)],
+                         [lambda x: (9 * np.square(x) * y3) / (4 * x3 ** 2),
+                          lambda x: -(y3 / 4) + (3 * x * y3) / (2 * x3),
+                          lambda x: -((5 * y3) / 4) - (9 * np.square(x) * y3) /
+                                    (4 * x3 ** 2) + (9 * x * y3) / (2 * x3)])
+        y = y.round(decimals=3)
+
+        if target < currentnow:
+            print('Ramp is down!')
+            yy = np.flip(y) + target
+        if target > currentnow:
+            print('Ramp is up!')
+            yy = y + currentnow
+
+        plt.ion()
+        plt.plot(x, yy)
+
+        return dwell, yy
+
+    def calcLinTrajectorie(self):
+        print('Calculating current trajectorie!')
+        if self.checkBox_connect.isChecked() == True:
+            print('Meausring current!')
+            currentnow = self.supply.measureCurrent(channel=2)
+        else:
+            currentnow = 0.
+
+        target = self.doubleSpinBox_lintarget.value()
+        y3 = abs(currentnow - target)
+        rate = self.doubleSpinBox_linrate.value() / 60.
+        dwell = self.spinBox_lindwell.value() / 1000.
+
+        x3 = y3 / rate  # ramp time
+        reso = x3 / dwell
+
+        x = np.linspace(0, x3, int(reso))
+        y = rate * x
+        y = y.round(decimals=3)
+
+        if target < currentnow:
+            print('Ramp is down!')
+            yy = np.flip(y) + target
+        if target > currentnow:
+            print('Ramp is up!')
+            yy = y + currentnow
+        if target == currentnow:
+            print('Target current already set?')
+            yy = y
+
+        plt.ion()
+        plt.plot(x, yy)
+
+        return dwell, yy
 
 
 class Worker(QRunnable):
